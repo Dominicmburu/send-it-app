8ae6f57e59830cf48a47925c8fe1ff0676d73dd8
Dominicmburu <dominicmburu034@gmail.com>
2025-02-13 10:11:45 +0300

    ch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-<<<<<<< HEAD
 import AdminDashboard from "./pages/AdminPage";
 import UserDashboard from "./pages/UserPage";
 import LoginPage from "./pages/LoginPage";
@@ -14,14 +13,6 @@ import IndexPage from "./pages/IndexPage";
 import AllParcels from "./pages/AllParcels";
 import ParcelDetails from "./pages/ParcelDetails";
 import MapPage from "./pages/MapPage";
-=======
-import AdminDashboard from "./AdminPage";
-import UserDashboard from "./UserPage";
-import LoginPage from "./LoginPage";
-import SignupPage from "./SignupPage";
-import IndexPage from "./IndexPage";
-import AllParcels from "./AllParcels";
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
 
 const ProtectedRoute = ({ children, role }) => {
   const token = localStorage.getItem("token");
@@ -46,7 +37,6 @@ const App = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<IndexPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -63,17 +53,11 @@ const App = () => {
 
         {/* <Route
           path="/admin-dashboard"
-=======
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/admin-dashboard/"
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
           element={
             <ProtectedRoute role="admin">
               <AdminDashboard />
             </ProtectedRoute>
           }
-<<<<<<< HEAD
         /> */}
         {/* <Route
           path="/admin-dashboard/parcels/:parcelId"
@@ -84,36 +68,21 @@ const App = () => {
           }/> */}
 
         {/* <Route
-=======
-        />
-        <Route
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
           path="/admin-dashboard/parcels"
           element={
             <ProtectedRoute role="admin">
               <AllParcels/>
             </ProtectedRoute>
           }
-<<<<<<< HEAD
         /> */}
         {/* <Route
-=======
-        />
-        <Route
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
           path="/user-dashboard"
           element={
             <ProtectedRoute role="user">
               <UserDashboard />
             </ProtectedRoute>
           }
-<<<<<<< HEAD
         /> */}
-=======
-        />
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/signup" element={<SignupPage />} />
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
       </Routes>
     </Router>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './styles/index.css'
 import App from './App'
-=======
-import './index.css'
-import App from './App.jsx'
->>>>>>> 5befa322306a6ce5631946bdb3a2ba248b8366e2
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
