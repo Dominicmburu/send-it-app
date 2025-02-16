6dad0163da1e594fc06c3a249514cb4778b8294f
jsonwamuyu <jysonmuchiri@gmail.com>
2025-02-16 17:32:20 +0300

    Set up the cypress testing environment

--- frontend/cypress/e2e/welcome-page.cy.js
+++ frontend/cypress/e2e/welcome-page.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+  beforeEach(()=>{
     cy.visit('http://localhost:5173')
+  })
+  it('Show welcome message', () => {
+    cy.get('h1').contains('Welcome')
+  })
+  it('should have a link to login page', ()=>{
+    cy.get('Link').contains('Get Started NOW!')
   })
 })
